f492997664471ede68357d08b5ce267868c86ba6
Pablo Torres <pablo.torres@insight-centre.org>
2017-03-08 16:16:36 +0000

    basic example ready

--- task4/src/test/scala/tutorial/mr/task4/Example.scala
+++ task4/src/test/scala/tutorial/mr/task4/Example.scala
@@ -8,6 +8,7 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
 import org.apache.spark.graphx._
+import org.apache.spark.graphx.lib.ShortestPaths
 import org.apache.spark.rdd.RDD
 import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
@@ -36,7 +37,6 @@ class Example extends AssertionsForJUnit {
       )
   
   @Test def basicGraph() {
-      
     using(new SparkContext(new SparkConf().setAppName("embedded").setMaster("local[2]"))) { sc =>
       val vertexRDD: RDD[(Long, (String, Int))] = sc.parallelize(nodes)
       val edgeRDD: RDD[Edge[Int]] = sc.parallelize(edges)
@@ -86,8 +86,7 @@ class Example extends AssertionsForJUnit {
     } 
   }
   
-  @Test def statistics() {
-    
+  @Test def statistics() {  
     using(new SparkContext(new SparkConf().setAppName("embedded").setMaster("local[2]"))) { sc =>
       val vertexRDD: RDD[(Long, (String, Int))] = sc.parallelize(nodes)
       val edgeRDD: RDD[Edge[Int]] = sc.parallelize(edges)
@@ -95,14 +94,38 @@ class Example extends AssertionsForJUnit {
       
       /* Graph statistics */
       logger.info ("")
-      logger.info ("==> Indegree average <==")
+      logger.info ("==> In-degree average <==")
       val in = graph.inDegrees.map{case (_, value) => (value, 1)}
         .reduce{case ( c1, c2) => (c1._1 + c2._1, c1._2 + c2._2) }
-      logger.info(s"Indegree average ${in._1/in._2}")
+      logger.info(s"In-degree average ${in._1/in._2}")
       
       logger.info ("")
-      logger.info ("==> Average path <==")
-       //val ave = graph.
+      logger.info ("==> Maximum indegree <==")
+      val max = graph.inDegrees.reduce( {case (a, b) => if (a._2 > b._2) a else b } )._1
+      val name = graph.vertices.lookup(max).collectFirst({ case a => a._1})
+      logger.info (s"Maximum in-degree ${name}")
+      
+      logger.info ("")
+      logger.info ("==> Shortest path v1, v5 <==")
+      val v1 = graph.vertices.collect {case t if t._2._1 == "Alice" => t._1}.first()
+      val v5 = graph.vertices.collect {case t if t._2._1 == "Ed" => t._1}.first()
+      val sh = ShortestPaths.run(graph, Seq(v1))
+      val path = sh.vertices
+        .filter( { case(vId, _) => vId == v5 } )
+        .first
+        //._2
+        //.get(v1)
+      logger.info (s"Shortest Path ${path}")
+      
+      /* Domain statistics */
+      logger.info ("")
+      logger.info ("==> Average age <==")
+      graph.collectNeighbors(EdgeDirection.Either)
+        .mapValues(t => t.map(_._2._2).sum / t.length.toDouble)
+        .collect()
+        .foreach {
+          case (id, ave) => logger.info (s"For node ${id} the neighbour average age is ${ave}")
+      }
     }
   }
 }
